96d4de10ef850f790875a175c33a2d125b59ac79
Kunal Kankariya <kkankariya007@gmail.com>
2023-04-20 08:43:14 +0530

    Hogaya Bhai

---
+++ Movie Ticket Booking System/test/movie/ticketbooking/system/BookTicketsFrmTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movie.ticketbooking.system;
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import org.junit.*;
+
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assert.*;
+
+/**
+ *
+ * @author Kunal
+ */
+public class BookTicketsFrmTest {
+    
+    public BookTicketsFrmTest() {
+    }
+    
+    @BeforeClass 
+    public static void setUpClass() {
+    }
+    
+    @AfterClass 
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class BookTicketsFrm.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        BookTicketsFrm.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
